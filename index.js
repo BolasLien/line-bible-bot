b3a06c95be635de69b41d8aaa79ba919ea2376af
BolasLien <ltzuen@gmai.com>
2020-04-28 15:54:39 +0800

    機器人

---
+++ index.js
@@ -0,0 +1,33 @@
+// 引用 linebot 套件
+import linebot from 'linebot'
+// 引用 dotenv 套件
+import dotenv from 'dotenv'
+// 引用 request 套件
+import rp from 'request-promise'
+
+// 讀取 .env 檔
+dotenv.config()
+
+// 宣告機器人資訊
+const bot = linebot({
+  channelId: process.env.CHANNEL_ID,
+  channelSecret: process.env.CHANNEL_SECRET,
+  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN
+})
+
+// 當收到訊息時
+bot.on('message', async (event) => {
+  let msg = ''
+  try {
+    const data = await rp({ uri: 'https://kktix.com/events.json', json: true })
+    msg = data.entry[0].title
+  } catch (error) {
+    msg = '發生錯誤'
+  }
+  event.reply(msg)
+})
+
+// 在 port 啟動
+bot.listen('/', process.env.PORT, () => {
+  console.log('機器人已啟動')
+})
