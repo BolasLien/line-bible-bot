d4b45e257bd73dabc635a4172ab7952374eb305f
BolasLien <ltzuen@gmai.com>
2020-05-04 13:23:16 +0800

    查信望愛的聖經

--- index.js
+++ index.js
@@ -15,14 +15,36 @@ const bot = linebot({
   channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN
 })
 
+const findChap = (str) => {
+  const s = str.split(' ')
+  console.log(s)
+
+  const json = {
+    chineses: s[0],
+    chap: s[1],
+    version: 'nstrunv',
+    sec: s.length === 3 ? s[2] : 0,
+    gb: 0
+  }
+
+  console.log(json)
+  return json
+}
+
 // 當收到訊息時
 bot.on('message', async (event) => {
+  console.log(event.message.text)
+
   let msg = ''
   try {
-    const data = await rp({ uri: 'https://kktix.com/events.json', json: true })
-    msg = data.entry[0].title
+    const data = await rp({
+      uri: 'https://bible.fhl.net/json/qb.php',
+      qs: findChap(event.message.text),
+      json: true
+    })
+    msg = data.record[0].bible_text
   } catch (error) {
-    msg = '發生錯誤'
+    msg = '發生錯誤' + error
   }
   event.reply(msg)
 })
